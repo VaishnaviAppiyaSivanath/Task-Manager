b324e9c62068ed24df958ae51e41edabe26610f0
VaishnaviAppiyaSivanath <vaishnavishivanath@gmail.com>
2024-10-16 17:38:19 +0100

    Fixes and improvements
    Moved from flow to apex for delete action
    Moved from LDS to imperative as object is not supported

--- Task Manager/force-app/main/default/lwc/taskManager/taskManager.js
+++ Task Manager/force-app/main/default/lwc/taskManager/taskManager.js
@@ -2,6 +2,8 @@ import { LightningElement, track, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getTaskList from '@salesforce/apex/TaskController.getTasks';
+import deleteTasks from '@salesforce/apex/TaskController.deleteTasks';
+import updateTaskStatus from '@salesforce/apex/TaskController.updateTasks';
 
 export default class TaskManager extends LightningElement {
     tasks;
@@ -11,26 +13,13 @@ export default class TaskManager extends LightningElement {
     taskToEdit = null;
     hasNoTask = false;
     isDelete = false;
+    taskIds = [];
     // Task columns
     columns = [
         { label: 'Subject', fieldName: 'Subject' },
         { label: 'Status', fieldName: 'Status', type: 'text' },
         { label: 'Priority', fieldName: 'Priority', type: 'text' },
-        { label: 'Due Date', fieldName: 'ActivityDate', type: 'date' },
-        {
-            type: 'button', typeAttributes: {
-                label: 'Complete',
-                name: 'complete',
-                variant: 'success'
-            }
-        },
-        {
-            type: 'button', typeAttributes: {
-                label: 'Delete',
-                name: 'delete',
-                variant: 'destructive'
-            }
-        }
+        { label: 'Due Date', fieldName: 'ActivityDate', type: 'date' }
     ];
 
     // Fetch task list from the server
@@ -41,56 +30,66 @@ export default class TaskManager extends LightningElement {
             this.hasNoTask = true;
         } else if (result.error) {
             this.error = result.error;
-            this.tasks = undefined;
+            console.error(result.error);
         }
     }
 
     // Toggle form visibility for creating a new task
-    toggleFormVisibility() {
+    createTask() {
         this.showForm = true;
-        //this.taskToEdit = null;
     }
 
     deleteRecord() {
         var el = this.template.querySelector('lightning-datatable');
-        console.log(el);
-        var selected = el.getSelectedRows();
-        console.log(selected);
+        
+        var selectedRows = el.getSelectedRows();
+        
+        selectedRows.forEach((record) => {this.taskIds.push(record.Id)});
+        
         this.isDelete = true;
+        deleteTasks({taskIds: this.taskIds})
+        .then((result) => {
+            this.showToast('Task Deleted', 'Task has been deleted successfully', 'success');
+            refreshApex(this.tasks); // Refresh the task list
+        })
+        .catch(error => {
+            console.error(error);
+        })
+            
     }
-
+    
     handleStatusChange(event) {
         if (event.detail.status === 'FINISHED') {
             // set behavior after a finished flow interview
             this.showToast('Task Created', 'Task has been created successfully', 'success');
-            this.showForm = false; // Hide the form
             refreshApex(this.tasks); // Refresh the task list
-        }
-    }
-    
-
-    // Handle row actions (Complete or Delete)
-    handleRowAction(event) {
-        const actionName = event.detail.action.name;
-        const row = event.detail.row;
-
-        if (actionName === 'complete') {
-            this.updateTaskStatus(row.Id, 'Completed');
-        } else if (actionName === 'delete') {
-            this.deleteTask(row.Id);
+            this.showForm = false; // Hide the form
+            this.hasNoTask = false;
         }
     }
 
     // Complete the task by updating its status
-    updateTaskStatus(taskId, status) {
-        // set flow input variables
+    markComplete() {
+        var el = this.template.querySelector('lightning-datatable');
+        var selectedRows = el.getSelectedRows();
+        selectedRows.forEach((record) => {this.taskIds.push(record.Id)});
+        updateTaskStatus({taskIds: this.taskIds})
+        .then((result) => {
+            this.showToast('Task Updated', 'Task has been updated successfully', 'success');
+            refreshApex(this.tasks); // Refresh the task list
+        })
+        .catch(error => {
+            console.error(error);
+        })
     }
 
-    
-    deleteTask(taskId) {
-        // set flow input variables
+    getSelectedRowIds() {
+        var selectedIds = [];
+        var el = this.template.querySelector('lightning-datatable');
+        var selectedRows = el.getSelectedRows();
+        selectedRows.forEach((record) => {this.selectedIds.push(record.Id)});
+        return selectedIds;
     }
-
     // Show toast notifications
     showToast(title, message, variant) {
         const evt = new ShowToastEvent({
